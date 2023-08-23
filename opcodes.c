b99bb9eac7b7f1f37fa26d30d20e47e435bda30c
meriembenayad <meriem.benayad@gmail.com>
2023-08-23 13:17:55 +0100

    MANDATORY TASKS

--- opcodes.c
+++ opcodes.c
@@ -77,33 +77,3 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
-
-void add(stack_t **stack, unsigned int line_number)
-{
-	if (!stack || !*stack || !(*stack)->next)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->next->n += (*stack)->n;
-	pop(stack, line_number);
-}
-
-void sub(stack_t **stack, unsigned int line_number)
-{
-	if (!stack || !*stack || !(*stack)->next)
-	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	(*stack)->next->n -= (*stack)->n;
-	pop(stack, line_number);
-}
-
-void nop(stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
-}
---
+++ opcodes_1.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
