61ddc60f76c22456a1dc4b1cf656e0bf672589bc
meriembenayad <meriem.benayad@gmail.com>
2023-08-25 00:40:23 +0100

    FIX betty main.c

--- stack_functions_2.c
+++ stack_functions_2.c
@@ -65,22 +65,21 @@ void sub(stack_t **topstack, uint line_number)
 
 void _div(stack_t **topstack, uint line_number)
 {
-	stack_t *tmp = *topstack;
 
-	if (tmp == NULL || tmp->next == NULL)
+	if (*topstack == NULL || (*topstack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't _div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	if (tmp->n == 0)
+	if ((*topstack)->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	tmp->next->n /= tmp->n;
-	*topstack = tmp->next;
+	(*topstack)->next->n /= (*topstack)->n;
+	*topstack = (*topstack)->next;
 	pop(topstack, line_number);
 	free(topstack);
 }
